a81585304867cc7886dc36c56552902874cc3fd5
mmaki12 <mmaki12@hotmail.com>
2023-10-20 11:41:27 +0100

    muhanad

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let  user = document.querySelector("#User")
 let pass = document.querySelector("#pass")
 function myFunction(){
     if (user === "muhanad" && pass === "maki"){
-        location.replace("/Home.html")
+        location.replace = URL("/Home.html")
     }
 };
  
