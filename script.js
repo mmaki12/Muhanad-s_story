91ab82998833b55f316c79ce1dec44278b9defce
mmaki12 <mmaki12@hotmail.com>
2023-10-20 11:24:42 +0100

    muhanad

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let  user = document.querySelector("#User")
+let pass = document.querySelector("#pass")
+function myFunction(){
+    if (user === "muhanad" && pass === "maki"){
+        location.replace("/Home.html")
+    }
+};
+ 
